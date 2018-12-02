5fd42c8b37064a596c4e2093dbee8c7a00f83d67
quinnmarsh <45250881+quinnmarsh@users.noreply.github.com>
2018-12-02 13:39:03 -0800

    Update rv2coes.m

--- rv2coes.m
+++ rv2coes.m
@@ -1,5 +1,4 @@
 %% r and v to COEs function
-
 function Orbit = rv2coes(r,v,mu)
 %% angular velocity [km^2/s]
 h_vec = cross(r,v);
@@ -20,16 +19,19 @@ if isnan(RAAN) %deal with /0
 end
 Orbit.RAAN = RAAN;
 
-%% Radial Velocity
+%% Velocity (Radial and Azmuthal)
 V_r = dot(r/norm(r),v);
+V_az = cross(r/norm(r),v);%???????unconfirmed equation 
 
 %% Ecc
 e_vec = (1/mu)*(r.*(norm(v)^2-mu/norm(r)) - (norm(r)*V_r).*v);
-e = norm(e_vec);
-Orbit.e = e;
+ecc = norm(e_vec);
+Orbit.ecc = ecc;
+Orbit.apse = e_vec/ecc;
+
 
 %% ArgPeri
-ArgP = acosd(dot(n/norm(n),e_vec/Orbit.e));
+ArgP = acosd(dot(n/norm(n),e_vec/ecc));
 if e_vec(3) < 0 %Quadrant Fix
     ArgP = 360 - ArgP;
 end
@@ -39,7 +41,7 @@ end
 Orbit.ArgP = ArgP;
 
 %% True Anamoly
-TA = acosd(dot(e_vec/Orbit.e,r/norm(r)));
+TA = acosd(dot(e_vec/ecc,r/norm(r)));
 if V_r < 0
     TA = 360 - TA;
 end
@@ -53,7 +55,7 @@ Energy = (norm(v)^2/2 - mu/norm(r));
 Orbit.Energy = Energy;
 
 %% Semi-Major Axis [km]
-a = h^2/mu/(1-e^2);%orbit equation special case
+a = h^2/mu/(1-ecc^2);%orbit equation special case
 Orbit.a = a;
 
 %% Period [seconds]
@@ -65,16 +67,21 @@ n = 2*pi/P;
 Orbit.n = n;
 
 %% t_0 better explaination to come
-E_0 = 2*atan(tand(TA/2)/sqrt((1+e)/(1-e)));%Eccentic Anamoly [radians]
-t_0 = (1/n)*(E_0-e*sin(E_0));
+E_0 = 2*atan(tand(TA/2)/sqrt((1+ecc)/(1-ecc)));%Eccentic Anamoly [radians]
+t_0 = (1/n)*(E_0-ecc*sin(E_0));
 Orbit.E_0 = E_0;
 Orbit.t_0 = t_0;
 
+%% Mean Anomoly [radians]
+Me = E_0 + ecc*sin(E_0);
+Orbit.Me = Me;
+
 %% Radius and Velocity at Periapse and Apoapse
-Orbit.rp = h^2/mu/(1+e);
-Orbit.ra = h^2/mu/(1-e);
+Orbit.rp = h^2/mu/(1+ecc);
+Orbit.ra = h^2/mu/(1-ecc);
 Orbit.vp = h/Orbit.rp;
 Orbit.va = h/Orbit.ra;
 
 end
+
 
