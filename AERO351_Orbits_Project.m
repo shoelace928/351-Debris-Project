81a2255b8571f6137eaa7f478160666799df2209
shoelace928 <44529570+shoelace928@users.noreply.github.com>
2018-12-04 15:15:46 -0800

    Includes transfer for 3 to 4

--- AERO351_Orbits_Project.m
+++ AERO351_Orbits_Project.m
@@ -28,18 +28,22 @@ mu = 398600 ; %gravitational constant for earth
 
 %% TLE.txt Upload and Conversion
         %tle need to be in txt file
-tle = load('Breeze1_tle.txt') ;    %Breeze Rocket Debris at 50.1919 inc in LEO
+tle = load('Breeze2_tle.txt') ;    %Breeze Rocket Debris at 50.1919 inc in LEO
 [inc1, epoch1, RAAN1, ecc1, arg1, Me1, n1] = tle_convert(tle) ;
-[rSat1, vSat1] = TLE_State(RAAN1,arg1,Me1,n1,inc1,ecc1) ;
-tle = load('Breeze2_tle.txt') ;    %Breeze Rocket Debris at 50.0668 inc in LEO
+[irSat1, ivSat1] = TLE_State(RAAN1,arg1,Me1,n1,inc1,ecc1) ;
+iSat1State = [irSat1(1), irSat1(2), irSat1(3), ivSat1(1), ivSat1(2), ivSat1(3)];
+tle = load('Breeze1_tle.txt') ;    %Breeze Rocket Debris at 50.0668 inc in LEO
 [inc2, epoch2, RAAN2, ecc2, arg2, Me2, n2] = tle_convert(tle) ;
-[rSat2, vSat2] = TLE_State(RAAN2,arg2,Me2,n2,inc2,ecc2) ;
+[irSat2, ivSat2] = TLE_State(RAAN2,arg2,Me2,n2,inc2,ecc2) ;
+iSat2State = [irSat2(1), irSat2(2), irSat2(3), ivSat2(1), ivSat2(2), ivSat2(3)];
 tle = load('Vanguard1_tle.txt') ;  %Iridium debris in MEO
 [inc3, epoch3, RAAN3, ecc3, arg3, Me3, n3] = tle_convert(tle) ;
-[rSat3, vSat3] = TLE_State(RAAN3,arg3,Me3,n3,inc3,ecc3) ;
+[irSat3, ivSat3] = TLE_State(RAAN3,arg3,Me3,n3,inc3,ecc3) ;
+iSat3State = [irSat3(1), irSat3(2), irSat3(3), ivSat3(1), ivSat3(2), ivSat3(3)];
 tle = load('Kizuna_tle.txt')  ;    %Kizuna debris in GEO
 [inc4, epoch4, RAAN4, ecc4, arg4, Me4, n4] = tle_convert(tle) ;
-[rSat4, vSat4] = TLE_State(RAAN4,arg4,Me4,n4,inc4,ecc4) ;
+[irSat4, ivSat4] = TLE_State(RAAN4,arg4,Me4,n4,inc4,ecc4) ;
+iSat4State = [irSat4(1), irSat4(2), irSat4(3), ivSat4(1), ivSat4(2), ivSat4(3)];
 
 %launch date: December 6, 2018 0h UT
         m_ld = 12 ;
@@ -59,9 +63,198 @@ tle = load('Kizuna_tle.txt')  ;    %Kizuna debris in GEO
         
         delta_t = (JD_ld - JD)*24*3600 ;
         delta_t = [delta_t(3) delta_t(2) delta_t(1) delta_t(4)] ;
-        tspan = delta_t;
-[rSat1, vSat1, rSat2, vSat2, rSat3, vSat3, rSat4, vSat4] = propagator(rSat1, vSat1, rSat2, vSat2, rSat3, vSat3, rSat4, vSat4, tspan, mu) ;
+        tspan = [1.061e6 1.058e6 3.7589e5 1.3699e6] ;
+        
+tspan1 = [0 tspan(2)];
+options = odeset('RelTol', 1e-8, 'AbsTol', 1e-8);
+[tSat1, Sat1State] = ode45(@twobodymotion, tspan1, iSat1State, options, mu);
+rSat1 = Sat1State(:,1:3);
+vSat1 = Sat1State(:,4:6);
 
+%Sat2:
+tspan2 = [0 tspan(1)];
+options = odeset('RelTol', 1e-8, 'AbsTol', 1e-8);
+[tSat2, Sat2State] = ode45(@twobodymotion, tspan1, iSat2State, options, mu);
+rSat2 = Sat2State(:,1:3);
+vSat2 = Sat2State(:,4:6);
+
+%Sat3:
+tspan3 = [0 (tspan(3)+(10*3600))];  %where satellite 3 is 10 hours after midnight
+options = odeset('RelTol', 1e-8, 'AbsTol', 1e-8);
+[ tSat3, Sat3State] = ode45(@twobodymotion, tspan1, iSat3State, options, mu);
+rSat3 = Sat3State(end,1:3);
+vSat3 = Sat3State(end,4:6);
+[h3, inc_degrees3, N3, RAAN_degrees3, ecc3, arg_degrees3, theta_degrees3] = coes (rSat3,vSat3,mu) ;
+rp3 = ((h3^2)/mu)*(1/(1+ecc3)) ;
+ra3 = ((h3^2)/mu)*(1/(1-ecc3)) ;
+a3 = (rp3+ra3)/2 ;
+T3 = ((2*pi)/sqrt(mu))*(a3^(3/2)) ;
+
+%Sat4:
+tspan4 = [ 0 ((5*T3) + tspan(4) + (10*3600))] ;   %where satellite 4 is 10 hours after midnight
+options = odeset('RelTol', 1e-8, 'AbsTol', 1e-8);
+[tSat4, Sat4State] = ode45(@twobodymotion, tspan1, iSat4State, options, mu);
+rSat4 = Sat4State(end,1:3);
+vSat4 = Sat4State(end,4:6);
+[h4, inc_degrees4, N4, RAAN_degrees4, ecc4, arg_degrees4, theta_degrees4] = coes (rSat4,vSat4,mu) ;
+
+fin1 = length(rSat1(:,1));
+fin2 = length(rSat2(:,1));
+fin3 = length(rSat3(:,1));
+fin4 = length(rSat4(:,1));
+
+% figure(1)
+% hold on
+% earth_sphere
+% plot3(rSat1(:,1),rSat1(:,2),rSat1(:,3))
+% plot3(rSat1(fin1,1), rSat1(fin1,2), rSat1(fin1,3),'o')
+% plot3(rSat2(:,1),rSat2(:,2),rSat2(:,3))
+% plot3(rSat2(fin2,1), rSat2(fin2,2), rSat2(fin2,3),'o')
+% plot3(rSat3(:,1),rSat3(:,2),rSat3(:,3))
+% plot3(rSat3(fin3,1), rSat3(fin3,2), rSat3(fin3,3),'o')
+% plot3(rSat4(:,1),rSat4(:,2),rSat4(:,3))
+% plot3(rSat4(fin4,1), rSat4(fin4,2), rSat4(fin4,3),'o')
+
+%% Transfer for 3 to 4
+%inc/RAAN change
+r = norm(rSat4(end,1:3)) ;   %radius of geostationary orbit
+v_3 = sqrt(mu/r) ;  %orbital speed of sat 3
+
+alpha = acos((cos(inc3)*cos(inc4)) + (sin(inc3)*sin(inc4)*cos(RAAN4-RAAN3))) ;
+delta_v_planet34 = 2*v_3*sin(alpha/2) ;
+theta_4 = deg2rad(theta_degrees4) ;
+%Phasing 
+h_4 = sqrt(2*mu)*sqrt((r^2)/(2*r)) ;    %angular momentum of sat 4
+rp_3 = r ;  %chasing orbit, radius of perigee is equal to the radius of the geo orbit
+T1 = ((2*pi)/sqrt(mu)) * (r^(3/2)) ;    %period of sat 4
+E_4 = 2.*atan((sqrt((1-ecc4)/(1+ecc4)).*tan(theta_4/2))) ;
+t_34 = (T1/(2*pi)).*(E_4-(ecc4*sin(E_4))) ;  %flight time for where sat 4 is to get to perigee of sat 3/4 
+T2 = T1 - t_34 ;
+a_3 = ((T2.*sqrt(mu))./(2*pi)).^(2/3) ;    %semimajor axis of chasing orbit
+ra_3 = (2*a_3)-rp_3 ;   %radius of apogee for chasing orbit
+h_3 = sqrt(2*mu)*sqrt((ra_3.*rp_3)./(ra_3+rp_3)) ;    %angular momentum of chasing orbit
+
+v4_p = h_4./rp_3 ;   %speed of sat 4 at perigee
+v3_p = h_3./rp_3 ;   %speed of sat 3 in perigee in chasing orbit
+
+delta_v_drop = v3_p - v4_p ;
+delta_v_return = v4_p - v3_p ;
+delta_v_phasing34 = abs(delta_v_drop) + abs(delta_v_return) ;   %total delta v of phasing change
+delta_v_34 = delta_v_planet34 + delta_v_phasing34 ; %add delta v for hohmann transfer to this line
+
+
+function [xx,yy,zz] = earth_sphere(varargin)
+%EARTH_SPHERE Generate an earth-sized sphere.
+%   [X,Y,Z] = EARTH_SPHERE(N) generates three (N+1)-by-(N+1)
+%   matrices so that SURFACE(X,Y,Z) produces a sphere equal to 
+%   the radius of the earth in kilometers. The continents will be
+%   displayed.
+%
+%   [X,Y,Z] = EARTH_SPHERE uses N = 50.
+%
+%   EARTH_SPHERE(N) and just EARTH_SPHERE graph the earth as a 
+%   SURFACE and do not return anything.
+%
+%   EARTH_SPHERE(N,'mile') graphs the earth with miles as the unit rather
+%   than kilometers. Other valid inputs are 'ft' 'm' 'nm' 'miles' and 'AU'
+%   for feet, meters, nautical miles, miles, and astronomical units
+%   respectively.
+%
+%   EARTH_SPHERE(AX,...) plots into AX instead of GCA.
+% 
+%  Examples: 
+%    earth_sphere('nm') produces an earth-sized sphere in nautical miles
+%
+%    earth_sphere(10,'AU') produces 10 point mesh of the Earth in
+%    astronomical units
+%
+%    h1 = gca;
+%    earth_sphere(h1,'mile')
+%    hold on
+%    plot3(x,y,z)
+%      produces the Earth in miles on axis h1 and plots a trajectory from
+%      variables x, y, and z
+%   Clay M. Thompson 4-24-1991, CBM 8-21-92.
+%   Will Campbell, 3-30-2010
+%   Copyright 1984-2010 The MathWorks, Inc. 
+%% Input Handling
+[cax,args,nargs] = axescheck(varargin{:}); % Parse possible Axes input
+error(nargchk(0,2,nargs)); % Ensure there are a valid number of inputs
+% Handle remaining inputs.
+% Should have 0 or 1 string input, 0 or 1 numeric input
+j = 0;
+k = 0;
+n = 50; % default value
+units = 'km'; % default value
+for i = 1:nargs
+    if ischar(args{i})
+        units = args{i};
+        j = j+1;
+    elseif isnumeric(args{i})
+        n = args{i};
+        k = k+1;
+    end
+end
+if j > 1 || k > 1
+    error('Invalid input types')
+end
+%% Calculations
+% Scale factors
+Scale = {'km' 'm'  'mile'            'miles'           'nm'              'au'                 'ft';
+         1    1000 0.621371192237334 0.621371192237334 0.539956803455724 6.6845871226706e-009 3280.839895};
+% Identify which scale to use
+try
+    myscale = 6378.1363*Scale{2,strcmpi(Scale(1,:),units)};
+catch %#ok<*CTCH>
+    error('Invalid units requested. Please use m, km, ft, mile, miles, nm, or AU')
+end
+     
+% -pi <= theta <= pi is a row vector.
+% -pi/2 <= phi <= pi/2 is a column vector.
+theta = (-n:2:n)/n*pi;
+phi = (-n:2:n)'/n*pi/2;
+cosphi = cos(phi); cosphi(1) = 0; cosphi(n+1) = 0;
+sintheta = sin(theta); sintheta(1) = 0; sintheta(n+1) = 0;
+x = myscale*cosphi*cos(theta);
+y = myscale*cosphi*sintheta;
+z = myscale*sin(phi)*ones(1,n+1);
+%% Plotting
+if nargout == 0
+    cax = newplot(cax);
+    % Load and define topographic data
+    load('topo.mat','topo','topomap1');
+    % Rotate data to be consistent with the Earth-Centered-Earth-Fixed
+    % coordinate conventions. X axis goes through the prime meridian.
+    % http://en.wikipedia.org/wiki/Geodetic_system#Earth_Centred_Earth_Fixed_.28ECEF_or_ECF.29_coordinates
+    %
+    % Note that if you plot orbit trajectories in the Earth-Centered-
+    % Inertial, the orientation of the contintents will be misleading.
+    topo2 = [topo(:,181:360) topo(:,1:180)]; %#ok<NODEF>
+    
+    % Define surface settings
+    props.FaceColor= 'texture';
+    props.EdgeColor = 'none';
+    props.FaceLighting = 'phong';
+    props.Cdata = topo2;
+    % Create the sphere with Earth topography and adjust colormap
+    surface(x,y,z,props,'parent',cax)
+    colormap(topomap1)
+% Replace the calls to surface and colormap with these lines if you do 
+% not want the Earth's topography displayed.
+%     surf(x,y,z,'parent',cax)
+%     shading flat
+%     colormap gray
+    
+    % Refine figure
+    axis equal
+    xlabel(['X [' units ']'])
+    ylabel(['Y [' units ']'])
+    zlabel(['Z [' units ']'])
+    view(127.5,30)
+else
+    xx = x; yy = y; zz = z;
+end
+end
     %tle to state
     function [R,V] = TLE_State(RAAN,OMEGA,ME,MM,INC,ecc)
 
@@ -105,7 +298,7 @@ R = transpose(QxX*transpose(R_peri));
 V = transpose(QxX*transpose(V_peri));
 
 
-end
+    end 
     %Propagation
     function [rSat1, vSat1, rSat2, vSat2, rSat3, vSat3, rSat4, vSat4] = propagator(rSat1, vSat1, rSat2, vSat2, rSat3, vSat3, rSat4, vSat4, tspan, mu)
 %Propagates orbits over a specific amount of time
@@ -139,14 +332,14 @@ tspan4 = [ 0 tspan(4) ] ;
 rSat4 = Sat4State(:,1:3);
 vSat4 = Sat4State(:,4:6);
 
-rSat1 = rSat1(end,1:3);
-vSat1 = vSat1(end,1:3);
-rSat2 = rSat2(end,1:3);
-vSat2 = vSat2(end,1:3);
-rSat3 = rSat3(end,1:3);
-vSat3 = vSat3(end,1:3);
-rSat4 = rSat4(end,1:3);
-vSat4 = vSat4(end,1:3);
+% rSat1 = rSat1(end,1:3);
+% vSat1 = vSat1(end,1:3);
+% rSat2 = rSat2(end,1:3);
+% vSat2 = vSat2(end,1:3);
+% rSat3 = rSat3(end,1:3);
+% vSat3 = vSat3(end,1:3);
+% rSat4 = rSat4(end,1:3);
+% vSat4 = vSat4(end,1:3);
 
 end
 
@@ -341,6 +534,12 @@ z = dot(ecc,r)/(ecc1*r1) ;
     end 
 theta = z ;    %true anomaly in radians
 theta_degrees = theta * (180/pi) ;  %true anomaly in degrees
+    while theta_degrees < 0
+        theta_degrees = theta_degrees + 360 ;
+    end 
+    while theta_degrees > 360
+        theta_degrees = theta_degrees - 360 ;
+    end 
 
       end
       
@@ -355,42 +554,8 @@ theta_degrees = theta * (180/pi) ;  %true anomaly in degrees
         n = tle(2,8) ;    %mean motion at epoch 
       end 
       
+       
         
-      %% Quinn Code for Trey
-% % find common line between planes
-% N2 = cross(rSat2, vSat1)/norm(cross(rSat2, vSat2));
-% N3 = cross(rSat3, vSat3)/norm(cross(rSat3, vSat3));%normal to plane 3
-% Line23 = cross(N2,N3)/norm(cross(N2,N3));
-% PlaneChange23 = acosd(dot(N2,N3));
-% 
-% %True Anamolies on the apse line
-% TA2_apse = acosd(dot(Line23,orbit2.apse));
-% TA3_apse = acosd(dot(Line23,orbit3.apse));
-% 
-% %correction for goin the long way
-% crossy = cross(orbit2.apse,Line23);%sweep from perigee(last burn) to node (next burn)
-% if crossy(3)<0
-%     TA2_apse = 360 - TA2_apse;
-% end
-% 
-% %find time from perigee to apse line
-% time2burn = (TA2_apse*pi/180)/sqrt(mu)*orbit3.ra^(3/2);
-% 
-% %% align the planes (inc and raan change)
-% 
-% 
-% v2 = sqrt(mu/orbit3.ra)*[cosd(PlaneChange23) sind(PlaneChange23)];
-% dv = norm(v1-v2);
-
-
-% more code for trey
-% C32 = cosd(PlaneChange23)*eye(3) + (1-cos(PlaneChange23))*Line23'*Line23 - sind(PlaneChange23)*cross(Line23');
-% v2 = (C32*vSat2(end,1:3)')';
-% dv = norm(v2);
-% function wx = cross(w)
-% wx = [0 -w(3) w(2); w(3) 0 -w(1);-w(2) w(1) 0];
-% end
-
        
 
 
